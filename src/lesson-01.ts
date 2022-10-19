36a71d48084c54997562406e05562905dccbcbee
Daniel Biro <Daniel.Biro@evosoft.com>
2022-10-19 15:34:41 +0200

    4/4

--- src/lesson-01.ts
+++ src/lesson-01.ts
@@ -5,7 +5,7 @@ export default class calculator {
     // Hozd létre a következő változókat a megfelelő típusannotációval.
     
     /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
-
+    points: Array<number> = [1,2,3,4,5,6,7,8,9,10];
 
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy felhasználót reprezentál.
@@ -13,7 +13,10 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} users - minden elem egy felhasználó.
      */
-
+    users : Array<{name: string, email: string}> = [
+        {name: "Brad Pitt", email: "brad@holywood.com"},
+        {name: "Jack Nicholson", email: "frank@dull.com"}
+    ]
     
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy terméket reprezentál.
@@ -22,12 +25,14 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} products - minden elem egy termék.
      */    
-
+    products: Array<{name: string, price: number, stock: number, active?: boolean}> = [
+        { name: "zöld tea", price: 1100, stock: 10, active: false },
+        { name: "rooibos", price: 800, stock: 2 }
+    ]
     
     /** 
      * Inicializálj egy tuple típust, azaz adj neki értéket is.
      * @type {Tuple} role - number, string, boolean. 
      */
-
-
+    role : [ number, string, boolean ] = [ 4, "hello", false ];
 }
